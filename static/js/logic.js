d0ea1600b0ee7dc494411b1271855ab755101ef9
FalafelFriday <50255237+FalafelFriday@users.noreply.github.com>
2019-10-08 17:01:06 -0700

    Add files via upload

---
+++ static/js/logic.js
@@ -0,0 +1,45 @@
+const API_KEY = "pk.eyJ1IjoibWF4aW11c3IzeCIsImEiOiJjazBqc3VjMncwZWFxM2NvY3M0eTViYmgzIn0.2wFBm8vreIrwHtjmc1YeOA";
+
+var myMap = L.map("map", {
+    center: [40.7, -73.95],
+    zoom: 11
+  });
+  
+  // Adding tile layer to the map
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.pirates",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+
+var APILink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// var geojson;
+
+// Grab data with d3
+d3.json(APILink, function(response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].location;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+});
